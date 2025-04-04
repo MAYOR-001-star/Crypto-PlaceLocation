dc771b67d03b2c5fed09086b935952b9d812ccd9
baruwa abdul-azeez mayowa <mayorkunabdulazeez01@gmail.com>
2025-04-04 21:12:47 +0100

    just installed react router

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import './Home.css'
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="home">
+            <div className="hero">
+                <h1>Largest <br/> Crypto Marketplace</h1>
+            </div>
         </div>
     )
 }
